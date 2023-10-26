278dc6451feb86f4be049c64694c10f625987d67
abejgonzalez <abe.j.gonza@gmail.com>
2023-10-26 14:49:30 -0700

    Fix undriven signals

--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -1,7 +1,7 @@
 package memcpyacc
 
 import Chisel._
-import chisel3.{Printable}
+import chisel3.{Printable, DontCare}
 import freechips.rocketchip.tile._
 import org.chipsalliance.cde.config._
 import freechips.rocketchip.diplomacy._
@@ -15,7 +15,7 @@ class MemcpyAccel(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(
   override lazy val module = new MemcpyAccelImp(this)
 
   val queue_depth = p(QueueDepth)
-  
+
   val tapeout = p(HyperscaleSoCTapeOut)
   val roccTLNode = if (tapeout) atlNode else tlNode
 
@@ -25,7 +25,7 @@ class MemcpyAccel(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(
   val l2_memwriter =     LazyModule(new L2MemHelper("[memwriter]", numOutstandingReqs=32))
   roccTLNode := TLBuffer.chainNode(1) := l2_memwriter.masterNode
 }
-class MemcpyAccelImp(outer: MemcpyAccel)(implicit p: Parameters) 
+class MemcpyAccelImp(outer: MemcpyAccel)(implicit p: Parameters)
   extends LazyRoCCModuleImp(outer) with MemoryOpConstants {
 
   io.mem.req.valid := false.B
@@ -34,6 +34,9 @@ class MemcpyAccelImp(outer: MemcpyAccel)(implicit p: Parameters)
   io.mem.keep_clock_enabled := true.B
   io.interrupt := false.B
   io.busy := false.B
+  io.fpu_resp.ready := true.B
+  io.fpu_req.valid := false.B
+  io.fpu_req.bits := DontCare
 
   ////////////////////////////////////////
   ///// Don't touch above this line! /////
@@ -69,4 +72,4 @@ class MemcpyAccelImp(outer: MemcpyAccel)(implicit p: Parameters)
   outer.l2_memwriter.module.io.status.valid := cmd_router.io.dmem_status_out.valid
   outer.l2_memwriter.module.io.status.bits := cmd_router.io.dmem_status_out.bits.status
   io.ptw(1) <> outer.l2_memwriter.module.io.ptw
-} 
+}
