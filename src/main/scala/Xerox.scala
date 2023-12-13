2cfe06cafa1fb7ce3a06ca4bf372fa778cc02eb9
abejgonzalez <abe.j.gonza@gmail.com>
2023-12-12 16:58:42 -0800

    Checkpoint

--- src/main/scala/Xerox.scala
+++ src/main/scala/Xerox.scala
@@ -10,5 +10,13 @@ import roccaccutils.memstreamer._
 
 class Xerox(val logger: Logger = DefaultLogger)(implicit val p: Parameters, val hp: L2MemHelperParams) extends MemStreamer {
   lazy val io = IO(new MemStreamerBundle)
-  store_data_queue.io.enq <> load_data_queue.io.deq
+
+  io.mem_stream.req.bits := io.mem_stream.output_bytes_avail
+  store_data_queue.io.enq.bits.chunk_size_bytes := io.mem_stream.output_bytes_avail
+  store_data_queue.io.enq.bits.chunk_data := io.mem_stream.output_data
+  store_data_queue.io.enq.bits.is_final_chunk := io.mem_stream.output_last_chunk
+
+  // a bit counterintuitive (mem_stream.ready means mem_stream.req.bits bytes are ready to read out fron mem_stream.output_data)
+  store_data_queue.io.enq.valid := io.mem_stream.req.ready
+  io.mem_stream.req.valid := store_data_queue.io.enq.ready
 }
